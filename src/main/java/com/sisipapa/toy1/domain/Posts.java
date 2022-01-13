506fa3d354001d2e92e64f674e5397d3597d15a4
KIM_KYUNGHUN01 <kim_kyungnhun01@eland.co.kr>
2022-01-13 18:18:53 +0900

    주석추가

--- src/main/java/com/sisipapa/toy1/domain/Posts.java
+++ src/main/java/com/sisipapa/toy1/domain/Posts.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class Posts {
-    @Id // PK 필드
+    @Id // PK 필드-주석추가
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성규칙
     private Long id;
 
